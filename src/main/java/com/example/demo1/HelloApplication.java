decf445a048341bff82921d91e1f230e9d190d61
Manuel Landeros <victorlanderosm5@gmail.com>
2025-02-07 14:07:57 -0600

    Hola

---
+++ src/main/java/com/example/demo1/HelloApplication.java
@@ -0,0 +1,49 @@
+package com.example.demo1;
+
+import com.example.demo1.vistas.Calculadora;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    private VBox vBox;
+    private MenuBar mnbPrincipal;
+    private Menu menCompentencia1, menCompetencia2;
+    private MenuItem mitCalculadora;
+    private Scene escena;
+
+    void CrearUI(){
+        mitCalculadora = new MenuItem("Calculadora");
+        mitCalculadora.setOnAction(event -> new Calculadora());
+        menCompentencia1 = new Menu("Competencia 1");
+        menCompentencia1.getItems().addAll(mitCalculadora);
+        mnbPrincipal = new MenuBar();
+        mnbPrincipal.getMenus().addAll(menCompentencia1);
+        vBox = new VBox(mnbPrincipal);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        CrearUI();
+        stage.setTitle("Hola Mundo de Eventos :)");
+        stage.setScene(new Scene(vBox));
+        stage.show();
+        stage.setMaximized(true);
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+    void clickEvent(){
+        System.out.println("Evento desde un metodo :)");
+    }
+}
